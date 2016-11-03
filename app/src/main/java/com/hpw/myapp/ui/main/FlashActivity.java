c0024a75398c234fd1cec2d5127c52dc6b3d3d86
hpw123 <424346976@qq.com>
2016-11-03 12:59:58 +0800

    增加6.0权限申请

--- app/src/main/java/com/hpw/myapp/ui/main/FlashActivity.java
+++ app/src/main/java/com/hpw/myapp/ui/main/FlashActivity.java
@@ -1,15 +1,17 @@
 package com.hpw.myapp.ui.main;
 
+import android.Manifest;
 import android.os.Bundle;
 
 import com.hpw.mvpframe.base.CoreBaseActivity;
+import com.hpw.mvpframe.utils.helper.RxUtil;
 import com.hpw.myapp.R;
 import com.hpw.myapp.ui.home.HomeActivity;
+import com.tbruyelle.rxpermissions.RxPermissions;
 
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
 
 /**
  * Created by hpw on 16/10/28.
@@ -29,10 +31,13 @@ public class FlashActivity extends CoreBaseActivity {
     @Override
     public void initView(Bundle savedInstanceState) {
         Observable.timer(2000, TimeUnit.MILLISECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(aVoid -> {
-                    startActivity(HomeActivity.class);
-                    finish();
+                .compose(RxPermissions.getInstance(this).ensure(Manifest.permission.READ_PHONE_STATE))
+                .compose(RxUtil.rxSchedulerHelper())
+                .subscribe(granted -> {
+                    if (granted) {
+                        startActivity(HomeActivity.class);
+                        finish();
+                    }
                 });
     }
 }
